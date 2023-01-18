972884c625fbec4a982fd52141e0b5673fc4a263
Lokesh Ravichandru <lokesh.ravichandru@grootan.com>
2023-01-18 17:36:37 +0530

    Added formula for TM CLI

---
+++ Formula/tm.rb
@@ -0,0 +1,10 @@
+class Hello < Formula
+    desc "A simple greeter"
+    homepage "https://github.com/testmaan/tm-cli"
+    url "https://github.com//testmaan/tm-cli/releases/download/1.0.0/tm-macos-x64.tar.gz"
+    sha256 "39978c31442b7b39d3db552b88ce96f58464d22714b8000c6ac4792899befcf0"
+    version "1.0.0"
+    def install
+      bin.install "tm"
+    end
+  end
