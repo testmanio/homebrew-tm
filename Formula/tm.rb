7d84dddf025fd3da1cfb33f3c633e95d842229bc
lokeshgrootan <33007379+lokeshgrootan@users.noreply.github.com>
2023-01-19 16:26:29 +0530

    Update tm.rb

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,7 +1,7 @@
 class Tm < Formula
     desc "A simple greeter"
     homepage "https://github.com/testmaan/tm-cli"
-    url "https://github.com/testmaan/tm-cli/releases/download/1.0.1/tm-macos-x64.tar.gz"
+    url "https://github.com/testmaan/tm-cli/releases/download/1.0.1/tm-macos-x64-1-0-1.tar.gz"
     sha256 "c2efec6ff9bff3c9a3fa99cf5531944d76a91dfd9a59befe9a136a3bb1db3128"
     version "1.0.1"
     def install
