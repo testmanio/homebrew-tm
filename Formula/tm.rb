95f5dd703cc5c5d348ee394a6f6c8dbe5a6d77b2
lokeshgrootan <33007379+lokeshgrootan@users.noreply.github.com>
2023-09-26 17:05:24 +0530

    Update tm.rb

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,9 +1,9 @@
 class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmaan/homebrew-tm"
-    url "https://github.com/testmaan/homebrew-tm/releases/download/1.0.21/tm-macos-x64-1.0.21.tar.gz"
-    sha256 "4ac7c3132fb94b155922dd4da86dd0a3ea86d2684fef2ee56fce1dff64f20cd5"
-    version "1.0.21"
+    url "https://github.com/testmaan/homebrew-tm/releases/download/1.0.22/tm-macos-x64-1.0.22.tar.gz"
+    sha256 "469c7603f20301d94f0195e40d614d064efe7f76d509807eae5d462fbd10390c"
+    version "1.0.22"
     def install
       bin.install "tm"
     end
