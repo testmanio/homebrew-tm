12b6148fc7929c7edfb3afb74ed1a318a2ad787e
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2024-08-05 16:02:21 +0530

    Update tm.rb
    Version 1.4.0 - Added support to update test cases in an existing Playwright project without creating a fresh project. This enhancement eliminates the need for a complete project setup (like running npm install) for every change, streamlining the update process and saving time.

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,9 +1,9 @@
 class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmanio/homebrew-tm"
-    url "https://github.com/testmanio/homebrew-tm/releases/download/1.2.0/tm-macos-x64-1.3.0.tar.gz"
-    sha256 "0be83521b4849ca4b13a413fa5d6273820f2c4b5734b1beb2b1851a95627f755"
-    version "1.3.0"
+    url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.0/tm-macos-x64-1.4.0.tar.gz"
+    sha256 "ba807199787bb37b5af4599bdd198b2c84dad0f0a2a6f7e9f60f6b2dc321bb5c"
+    version "1.4.0"
     def install
       bin.install "tm"
     end
