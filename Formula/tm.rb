86de19bb7c7050b6fcfebb7add826f41a98b2b50
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2024-08-14 20:10:13 +0530

    Changed sha value for 1.4.1

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -2,7 +2,7 @@ class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmanio/homebrew-tm"
     url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.1/tm-macos-x64-1.4.1.tar.gz"
-    sha256 "97f4e9300943cb70e6c025d16119123fbc734ddb321f3d329b41dbab42b99493"
+    sha256 "8ec65f90959b7e64c5413b528a26c4852aa1b4212ecc0511ca5bbcdff71c847e"
     version "1.4.1"
     def install
       bin.install "tm"
