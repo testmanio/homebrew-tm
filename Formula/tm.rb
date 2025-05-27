f5768c84c5f1153e672b0c32b81820a120d12940
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2025-05-27 14:19:12 +0530

    Added global setup and global teardown

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,9 +1,9 @@
 class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmanio/homebrew-tm"
-    url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.5/tm-macos-x64-1.4.5.tar.gz"
-    sha256 "58889c37a2e012a7731b8e7125262c11c6961553dcdd49e2341d7b12c8caade7"
-    version "1.4.5"
+    url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.6/tm-macos-x64-1.4.6.tar.gz"
+    sha256 "50e0001de0ca97e991b9460de571b3976e6db23558abdefd83efee211160f954"
+    version "1.4.6"
     def install
       bin.install "tm"
     end
