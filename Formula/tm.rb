81934be87c00c746103589de8823af4ac6a48e4b
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2025-05-30 16:34:22 +0530

    version changed

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -3,7 +3,7 @@ class Tm < Formula
     homepage "https://github.com/testmanio/homebrew-tm"
     url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.8/tm-macos-x64-1.4.8.tar.gz"
     sha256 "a6c686fafb8cf21fac34c5ca01db96c3673edec1522b0d2bb272ac5c1f126e6b"
-    version "1.4.7"
+    version "1.4.8"
     def install
       bin.install "tm"
     end
