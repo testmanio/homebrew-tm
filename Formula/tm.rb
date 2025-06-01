b5ccb93d2a2d121cfc329c3b2e4a9287962d649a
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2025-06-01 19:52:04 +0530

    logs added global.js file

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -2,7 +2,7 @@ class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmanio/homebrew-tm"
     url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.8/tm-macos-x64-1.4.8.tar.gz"
-    sha256 "a6c686fafb8cf21fac34c5ca01db96c3673edec1522b0d2bb272ac5c1f126e6b"
+    sha256 "182e94100d41599ae6bac18d6da997aa609aba2ee020905ca58e00ff1cf5ec40"
     version "1.4.8"
     def install
       bin.install "tm"
