4db0542e2ce1d49c4a93d07e33480365e597a730
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2025-06-17 10:42:47 +0530

    updates playwright report paths

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -1,9 +1,9 @@
 class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmanio/homebrew-tm"
-    url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.10/tm-macos-x64-1.4.10.tar.gz"
-    sha256 "59c8f67c79882c089a70100f3ea29e5db2e1a3fe3d354aa023829e91a50af6c8"
-    version "1.4.10"
+    url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.11/tm-macos-x64-1.4.11.tar.gz"
+    sha256 "aff240a7c86d6327d48ec469940c9c09996c312217ca95fa051eb7a2e8b8522e"
+    version "1.4.11"
     def install
       bin.install "tm"
     end
