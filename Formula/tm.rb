685421ab3cebadc0fc06275c50b367fd29275032
NandhaSrinivasan <40190682+NandhaSrinivasan@users.noreply.github.com>
2025-06-18 07:23:41 +0530

    Update tm.rb

--- Formula/tm.rb
+++ Formula/tm.rb
@@ -2,7 +2,7 @@ class Tm < Formula
     desc "TestMan CLI Tool to generate automation test cases"
     homepage "https://github.com/testmanio/homebrew-tm"
     url "https://github.com/testmanio/homebrew-tm/releases/download/1.4.11/tm-macos-x64-1.4.11.tar.gz"
-    sha256 "0f3580a65fd861aa6ec63676dbc04968a27b16cec1bfe5ea116dc9eda68ac510"
+    sha256 "b82360301c32e32b475cf661319532f6a033a9bfcce0fe2dc54ebaa1e32a9765"
     version "1.4.11"
     def install
       bin.install "tm"
